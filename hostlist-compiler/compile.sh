c23f6fa4999c3fa840563505065eedcccff0630a
TrueConfiguration <35403031+TrueConfiguration@users.noreply.github.com>
2023-05-22 23:55:28 -0300

    Added comparison of files and .gitignore

--- hostlist-compiler/compile.sh
+++ hostlist-compiler/compile.sh
@@ -7,6 +7,7 @@ HOSTLIST_COMPILER=hostlist-compiler
 # Defines input and output paths
 CONFIGS_DIR=./configs
 COMPILED_DIR=./compiled
+TEMP_DIR=./temp
 
 # Loop files in ./configs
 for CONFIG_FILE in $CONFIGS_DIR/*.json; do
@@ -14,5 +15,29 @@ for CONFIG_FILE in $CONFIGS_DIR/*.json; do
   OUTPUT_FILE=$(basename "$CONFIG_FILE" .json | sed 's/.json//').txt
 
   # Runs hostlist-compiler with config and output parameters
-  $HOSTLIST_COMPILER -c "$CONFIG_FILE" -o "$COMPILED_DIR/$OUTPUT_FILE"
+  $HOSTLIST_COMPILER -c "$CONFIG_FILE" -o "$TEMP_DIR/$OUTPUT_FILE"
+done
+
+# Loop files in ./temp
+for file in "$TEMP_DIR"/*; do
+  filename=$(basename "$file")
+
+  # Checks if file already existis in ./compiled
+  if [ -e "$COMPILED_DIR/$filename" ]; then
+    # Compares files im ./temp and ./compiled
+    diff_output=$(diff "$file" "$COMPILED_DIR/$filename")
+
+    # Counts diff lines (because hostlist-compiler will always update created date to match fetch date)
+    num_lines_diff=$(echo "$diff_output" | sed -n '/^[0-9]/p' | wc -l)
+
+    # Checks if there is more than one line updated (date line)
+    if [ "$num_lines_diff" -gt 1 ]; then
+      cp "$file" "$COMPILED_DIR/$filename"
+      echo "The file $filename was copied to compiled folder."
+    fi
+  else
+    # If file doesn't exist, creates
+    cp "$file" "$COMPILED_DIR/$filename"
+    echo "The file $filename was created in compiled folder."
+  fi
 done
