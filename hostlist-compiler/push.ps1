dd122b97c80f8bc951fe445a43b4a33775630ca0
TrueConfiguration <35403031+TrueConfiguration@users.noreply.github.com>
2023-05-26 16:13:38 -0300

    Updated in 2023-05-26

--- hostlist-compiler/push.ps1
+++ hostlist-compiler/push.ps1
@@ -1,19 +1,19 @@
 # Fetch remote
-& "C:\Program Files\Git\cmd\git.exe" fetch
+& git fetch
 
 # Check for local changes
-$gitStatus = & "C:\Program Files\Git\cmd\git.exe" status --porcelain
+$gitStatus = & git status --porcelain
 
 if (-not [string]::IsNullOrWhiteSpace($gitStatus)) {
     # Add local changes
-    & "C:\Program Files\Git\cmd\git.exe" add .
+    & git add .
 
     # Set commit message
     $commit_message = "Updated in $((Get-Date).ToString('yyyy-MM-dd'))"
 
     # Commit changes
-    & "C:\Program Files\Git\cmd\git.exe" commit -S -m $commit_message
+    & git commit -S -m $commit_message
 
     # Push changes
-    & "C:\Program Files\Git\cmd\git.exe" push
+    & git push
 }
